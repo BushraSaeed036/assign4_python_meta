77029bbe43156a9d390ae657ec6d43a0e18f9a07
Bushra Saeed <bushrasaeed036@gmail.com>
2025-06-01 22:20:59 +0500

    Add files via upload

---
+++ assign4python_meta.py
@@ -0,0 +1,27 @@
+# Integer Example
+# these two lines will print height in feet, and weight in kg.
+height = 5
+weight = 50
+print("Height is:", height)
+print("Weight is:", weight)
+
+# String Example
+# these two lines will print the name of city and its description.
+city = "Faisalabad"
+description = "The city of beauty"
+print("City is:", city)
+print("Description:", description)
+
+# Float Example
+# these two lines will print length and width in cm.
+length = 10.12
+width = 20.6
+print("Length is:", length)
+print("Width is:", width)
+
+# Boolean Example
+# these two lines will print true or false according to given statement.
+is_brilliant_student = True
+is_fail = False
+print("Is Brilliant Student:", is_brilliant_student)
+print("Is Fail:", is_fail)
